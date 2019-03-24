c0b11f437a51f9fe22c568cb8eee3c3492e06a33
Marvin Zachary <acerzach@gmail.com>
2019-03-24 20:06:42 +0300

    Added an array to store my quote and included toggle details and delete function

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Quote } from '../quote';
 @Component({
   selector: 'app-quotes',
   templateUrl: './quotes.component.html',
@@ -7,9 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuotesComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit() {
+  quotes = [
+    new Quote(1, "I am the streets, you better look both ways before you cross me.", "ShortGun Suge",0, 0, "Zachary"),
+    new Quote(2, "Ambition is priceless that’s something thats in your veins.", "ROSS",0, 0, "Kidred"),
+    new Quote(3, "I was raised by the stop sign,No religion I was getting saved by the glock.", "Meek Mill", 0, 0,"Marvin")
+  ]
+  toggleDetails(index) {
+    this.quotes[index].showDetails = !this.quotes[index].showDetails;
   }
+  deleteQuote(isDelete, index) {
 
-}
+    if (isDelete) {
+      let toDelete = confirm("To go ahead please click ok, otherwise cancel.");
+      if (toDelete) {
+        this.quotes.splice(index, 1)
+      }
+    }
+  }
